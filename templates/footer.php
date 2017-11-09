9fff3f0dbd6bdfec09bd896cae9fad613b13151a
Jamaykos <Jamay.kos@gmail.com>
2017-11-09 21:47:36 +0300

    Подключил bootstrap.min.js. Теперь модальное окно должно открываться.

--- templates/footer.php
+++ templates/footer.php
@@ -2,6 +2,7 @@
 <script type="text/javascript">
     $(document).ready(function() {
 
+
     });
 </script>
 <script  type="text/javascript" src="<?php echo get_stylesheet_directory(); ?>/bootstrap/js/bootstrap.min.js"></script>
