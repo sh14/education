7ae3615cb9b1f57b73a5359b934429becd161a47
GekoCoolhard <gekocoolhard@inbox.ru>
2017-11-09 21:53:52 +0300

    модальные окна регистации

--- main.php
+++ main.php
@@ -5,9 +5,70 @@
  * Date: 07.11.2017
  * Time: 21:52
  */?>
-<div class="main container">
+<div class="main_container container">
     <div class="registrate">
-        <button class="main_login">Вход</button>
-        <button class="main_registration">Регистрация</button>
+        <div class="registration_buttons">
+            <button type="button" class="btn btn_main btn-success main_registration" data-toggle="modal" data-target="#modal-1" >Регистрация</button>
+            <button type="button" class="btn btn_main btn-success main_login" data-toggle="modal" data-target="#modal-2">Вход</button>
+        </div>
+        <br>
+        <br>
+        <div class="modal" id="modal-1">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Регистрация</h4>
+                        <button class="close" type="button" data-dismiss="modal">
+                            <i class="fa fa-close"></i>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form type="post">
+                            <p>Введите Ваше имя</p>
+                            <input type="text" placeholder="first_name" required="required">
+                            <br>
+                            <p>Введите Вашу фамилию</p>
+                            <input type="text" placeholder="last_name" required="required">
+                            <br>
+                            <p>Введите Ваш никнейм</p>
+                            <input type="text" placeholder="login" required="required">
+                            <br>
+                            <p>Введите Ваш email</p>
+                            <input type="email" placeholder="Email" required="required">
+                            <br>
+                            <p>Введите Ваш пароль</p>
+                            <input type="password" placeholder="Password" required="required">
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-primary" type="button">Зарегистрироваться</button>
+                        <button class="btn btn-danger" type="button" data-dismiss="modal">Да пошло оно все</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal" id="modal-2">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Вход</h4>
+                        <button class="close" type="button" data-dismiss="modal">
+                            <i class="fa fa-close"></i>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="login_window">
+                            <form class="login">
+                                <input class="login_input" placeholder="Логин"/>
+                                <input type="password" class="password_input" placeholder="Пароль"/>
+                                <button class="login">Вход</button>
+                                <button type="button" class="btn btn_main btn-success main_registration" data-toggle="modal" data-target="#modal-1">Регистрация</button>
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     </div>
 </div>
