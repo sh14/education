de656a91dd252d6c02b416bb25525fb2a90055d2
GekoCoolhard <GekoCoolhard@inbox.ru>
2017-11-14 18:30:08 +0300

    window_chat с кнопками профиля и выхода. (промежуточный результат)

--- main.php
+++ main.php
@@ -6,68 +6,70 @@
  * Time: 21:52
  */?>
 <div class="main_container container">
-    <div class="registrate">
-        <div class="registration_buttons">
-            <button type="button" class="btn btn_main btn-success main_registration" data-toggle="modal" data-target="#modal-1" >Регистрация</button>
-            <button type="button" class="btn btn_main btn-success main_login" data-toggle="modal" data-target="#modal-2">Вход</button>
-        </div>
-        <br>
-        <br>
-        <div class="modal" id="modal-1">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title">Регистрация</h4>
-                        <button class="close" type="button" data-dismiss="modal">
-                            <i class="fa fa-close"></i>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <form method="post">
-                            <p>Введите Ваше имя</p>
-                            <input type="text" placeholder="First name" required="required">
-                            <br>
-                            <br>
-                            <p>Введите Вашу фамилию</p>
-                            <input type="text" placeholder="Last name" required="required">
-                            <br>
-                            <br>
-                            <p>Введите Ваш логин</p>
-                            <input type="text" placeholder="login" required="required">
-                            <br>
-                            <br>
-                            <p>Введите Ваш email</p>
-                            <input type="email" placeholder="Email" required="required">
-                            <br>
-                            <br>
-                            <p>Введите Ваш пароль</p>
-                            <input type="password" placeholder="Password" required="required">
-                            <div class="modal-footer">
-                                <button class="btn btn-primary" type="submit">Зарегистрироваться</button>
-                                <button class="btn btn-danger" type="button" data-dismiss="modal">Да пошло оно все</button>
-                            </div>
-                        </form>
+    <div class="row">
+        <div class="registrate col-sm-12">
+            <div class="registration_buttons">
+                <button type="button" class="btn btn_main btn-success main_registration" data-toggle="modal" data-target="#modal-1" >Регистрация</button>
+                <button type="button" class="btn btn_main btn-success main_login" data-toggle="modal" data-target="#modal-2">Вход</button>
+            </div>
+            <br>
+            <br>
+            <div class="modal" id="modal-1">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title">Регистрация</h4>
+                            <button class="close" type="button" data-dismiss="modal">
+                                <i class="fa fa-close"></i>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form method="post">
+                                <p>Введите Ваше имя</p>
+                                <input type="text" placeholder="First name" required="required">
+                                <br>
+                                <br>
+                                <p>Введите Вашу фамилию</p>
+                                <input type="text" placeholder="Last name" required="required">
+                                <br>
+                                <br>
+                                <p>Введите Ваш логин</p>
+                                <input type="text" placeholder="login" required="required">
+                                <br>
+                                <br>
+                                <p>Введите Ваш email</p>
+                                <input type="email" placeholder="Email" required="required">
+                                <br>
+                                <br>
+                                <p>Введите Ваш пароль</p>
+                                <input type="password" placeholder="Password" required="required">
+                                <div class="modal-footer">
+                                    <button class="btn btn-primary" type="submit">Зарегистрироваться</button>
+                                    <button class="btn btn-danger" type="button" data-dismiss="modal">Да пошло оно все</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="modal" id="modal-2">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title">Вход</h4>
-                        <button class="close" type="button" data-dismiss="modal">
-                            <i class="fa fa-close"></i>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="login_window">
-                            <form class="login">
-                                <input class="login_input" placeholder="Логин" required="required"/>
-                                <input type="password" class="password_input" placeholder="Пароль" required="required"/>
-                                <button class="login btn btn-success" type="submit">Вход</button>
-                                <button type="button" class="btn btn_main btn-success main_registration" data-toggle="modal" data-target="#modal-1" data-dismiss="modal">Регистрация</button>
-                            </form>
+            <div class="modal" id="modal-2">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title">Вход</h4>
+                            <button class="close" type="button" data-dismiss="modal">
+                                <i class="fa fa-close"></i>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="login_window">
+                                <form class="login">
+                                    <input class="login_input" placeholder="Логин" required="required"/>
+                                    <input type="password" class="password_input" placeholder="Пароль" required="required"/>
+                                    <button class="login btn btn-success" type="submit">Вход</button>
+                                    <button type="button" class="btn btn_main btn-success main_registration" data-toggle="modal" data-target="#modal-1" data-dismiss="modal">Регистрация</button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
--- window_chat.php
+++ window_chat.php
@@ -8,23 +8,127 @@
 
 /*Обработка кнопки ВЫЙТИ*/
 if ( @$_POST['exit'] ) {
-	setcookie( 'RestrictedArea', '', time() - 60 * 60 * 24 );
-	header( "Location: " . $_SERVER['PHP_SELF'] );
-	exit();
+    setcookie( 'RestrictedArea', '', time() - 60 * 60 * 24 );
+    header( "Location: " . $_SERVER['PHP_SELF'] );
+    exit();
+}
+$message = 'Внесите данные, которые хотите изменить.';
+if ( ! empty( $_GET['event'] ) ) {
+    if ( $_GET['event'] == 'error' ) {
+        $message = 'Необходимо ввести все данные.';
+    }
+
+    if ( $_GET['event'] == 'success' ) {
+        $message = 'Запись добавлена.';
+    }
 }
 ?>
-<div class="container">
-    <h1>Окно чата</h1>
-    <div class="row row-centered post_form">
-        <div class="col-md-6">
-            <form action="" method="post">
-                <input type="text" class="form-control" placeholder="Тема сообщения">
-                <textarea class="form-control" rows="5" placeholder="Текст сообщения"></textarea>
-                <input class="btn btn-info" type="button" value="Отправить">
-            </form>
+<div class="chat_container container">
+    <div class="row">
+        <div class="registrate col-sm-12">
+            <div class="chat_profile_buttons">
+                <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+                    <button type="button" class="btn btn_chat btn-success btn_chat_profile" data-toggle="modal" data-target="#modal-1" >Мой профиль</button>
+                    <input type="submit" class="btn btn-danger btn_chat btn-success chat_logout" name="exit" value="Выход"/>
+                </form>
+            </div>
+            <br>
+            <br>
+            <div class="modal" id="modal-1">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title">Редактирование профиля</h4>
+                            <button class="close" type="button" data-dismiss="modal">
+                                <i class="fa fa-close"></i>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form method="post">
+                                <!-- Колонка аватара -->
+                                <form action="?event=upload" method="post" enctype="multipart/form-data">
+                                    <div class="row">
+                                        <div class="text-center col-md-3">
+                                            <div class="avatar col-md-12 " alt="avatar"></div>
+                                            <div class="file_upload col-md-12 col-xs-12">
+                                                <input type="file" name="file_to_upload" id="file_to_upload" accept="image/*">
+                                                <button type="button">Загрузить</button>
+                                            </div>
+                                            <button type="submit" value="upload_image" name="submit" class="btn btn-success">Отправить</button>
+                                        </div>
+                                    </div>
+                                </form>
+                                <!-- Колонка профиля -->
+                                <div class="row">
+                                    <div class="col-md-9 personal-info">
+                                        <h3>Личная информация</h3>
+                                        <form method="post" class="form-horizontal" role="form" action="?event=edit_user_info">
+                                            <div class="form-group">
+                                                <label class="col-lg-3 control-label">Имя:</label>
+                                                <div class="col-lg-8">
+                                                    <input class="form-control" type="text" name="first_name">
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="col-lg-3 control-label">Фамилия:</label>
+                                                <div class="col-lg-8">
+                                                    <input class="form-control" type="text" name="last_name">
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="col-lg-3 control-label">Email:</label>
+                                                <div class="col-lg-8">
+                                                    <input class="form-control" type="email" name="email">
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="col-md-3 control-label">Логин:</label>
+                                                <div class="col-md-8">
+                                                    <input class="form-control" type="text" name="login">
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="col-md-3 control-label">Пароль:</label>
+                                                <div class="col-md-8">
+                                                    <input class="form-control password" type="password" name="password">
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="col-md-3 control-label">Подтвердите пароль:</label>
+                                                <div class="col-md-8">
+                                                    <input class="form-control confirm_password" type="password">
+                                                </div>
+                                                <div class="message col-md-8 col-md-offset-3"></div>
+                                            </div>
+                                            <input type="hidden" name="access" value="permitted">
+                                            <input type="hidden" name="ID" value="1">
+                                            <div class="alert alert-info alert-dismissable">
+                                                <a class="panel-close close" data-dismiss="alert">×</a>
+                                                <i class="fa fa-coffee"><?php echo $message ?></i>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="col-md-3 control-label"></label>
+                                                <div class="col-md-8">
+                                                    <input type="submit" class="btn btn-primary" value="Сохранить изменения">
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h1>Окно чата</h1>
+            <div class="row row-centered post_form">
+                <div class="col-md-6">
+                    <form action="" method="post">
+                        <input type="text" class="form-control" placeholder="Тема сообщения">
+                        <textarea class="form-control" rows="5" placeholder="Текст сообщения"></textarea>
+                        <input class="btn btn-info" type="button" value="Отправить">
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
-    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
-        <input type="submit" class="btn btn-danger" name="exit" value="Выйти"/>
-    </form>
 </div>
