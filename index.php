3fc18c0968d92724c964038ce29eb899b6d5fa7a
Ruslan Ishmametov <russ-i@yandex.ru>
2017-11-15 23:08:39 +0300

    Изменения в функции verification_user

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 header( 'Content-Type: text/html; charset=utf-8' );
-
+setcookie('email','mail@mail.ru', time()+3600.);
+setcookie('password',md5('russ'), time()+3600.);
 require 'functions.php';
 require 'installer.php';
 init();
