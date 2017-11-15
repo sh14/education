a9686559dccde188bb9e2f63adf324447d7ae365
Anton <anton_123@inbox.ru>
2017-11-15 16:42:27 +0300

    Перещение функции регистрации в файл с функциями

--- functions.php
+++ functions.php
@@ -12,10 +12,6 @@ if ( empty( $link ) ) {
 	$link = mysqli_connect( HOST, LOGIN, PASSWORD, DATABASE );
 }
 
-//Отладка
-$result = '';
-
-
 function init() {
 	do_action( 'init' );
 	if ( ! empty( $_GET['p'] ) ) {
@@ -315,6 +311,45 @@ function autorization_user() {
 
 }
 
+/**
+ * Функция регистрации пользователя
+ */
+function registration() {
+	if (isset($_GET['event'])&&$_GET['event']=='registration'&&isset($_POST['email'])&&isset($_POST['password'])&&isset($_POST['action'])) {
+		$err = [];
+		echo 'Функция';
+		if (!preg_match("/^[a-zA-Z0-9]+$/", $_POST['email'])) {
+			$err = "Email может состоять только и букв английского языка";
+		}
+
+		if (strlen($_POST['email']) < 7 or strlen($_POST['email']) > 255) {
+			$err = "Email не должен быть меньше 7 символов и не больше 255";
+		}
+
+		$query = do_query("SELECT count(*) FROM users WHERE email='{$_POST['email']}'");
+
+		if (mysqli_num_rows($query) > 0) {
+
+			$err[] = "Пользователь с таким email существует";
+		}
+
+		if (count($err) == 0) {
+
+			$email = $_POST['email'];
+
+			$password = md5(md5(trim($_POST['password'])));
+
+			do_query("INSERT INTO users SET email='" . $email . "', password='" . $password . "'");
+			header("location:" . get_root_url());
+		} else {
+			echo "<strong>При регистрации произошли следующие ошибки:</strong><br>";
+			foreach ($err as $error) {
+				echo $error . "<br>";
+			}
+		}
+	}
+}
+add_action('init', 'registration');
 /*
  * это недоработанная функция сохраниня пользователя
  * function save_profile() {
