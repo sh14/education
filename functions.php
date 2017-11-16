3e01cb716bfcdef51edd5380905ae9673ebae7a0
Jamaykos <Jamay.kos@gmail.com>
2017-11-16 20:59:12 +0300

    функция регистрации

--- functions.php
+++ functions.php
@@ -320,15 +320,16 @@ function verification_user() {
  * Функция регистрации пользователя
  */
 function registration() {
-	if (isset($_POST['email'])&&isset($_POST['password'])&&isset($_POST['action'] =='registration')) {
+	if (!empty($_POST['email'])&&!empty($_POST['password'])&&!empty($_POST['action'] =='registration')) {
 		$err = [];
-		if ( ! preg_match( "/^[a-zA-Z0-9]+$/", $_POST['email'] ) ) {
-			$err = "Email может состоять только и букв английского языка";
-		}
 
 		if ( strlen( $_POST['email'] ) < 7 or strlen( $_POST['email'] ) > 255 ) {
-			$err = "Email не должен быть меньше 7 символов и не больше 255";
+			$err[] = "Email не должен быть меньше 7 символов и не больше 255";
 		}
+
+		if (strlen( $_POST['password']) < 6 or strlen( $_POST['password']) > 255) {
+		    $err[] = "Password не должен быть меньше 6 символов и не больше 255";
+        }
 
 		$query = do_query( "SELECT count(*) FROM users WHERE email='{$_POST['email']}'" );
 
