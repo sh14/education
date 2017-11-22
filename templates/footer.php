b79d581701e640d167818bfee34efbed1b7eec48
Johnny-A <mtv-gio@mail.ru>
2017-11-22 19:31:54 +0300

    Merge branch 'master' of https://github.com/sh14/education

--- templates/footer.php
+++ templates/footer.php
@@ -18,6 +18,22 @@
             }
         });
     });
+    /*Кнопка выхода*/
+    function preparePage() {
+      document.getElementById('one').onclick = function () {
+        if (document.getElementById('one')) {
+            document.getElementById('two').style.display = "block";
+            document.getElementById('three').style.display = "none";
+            document.getElementById('one').style.display = "none";
+      } else {
+        document.getElementById('two').style.display = "none";
+      }
+    };
+    document.getElementById('two').style.display = "none";
+  }
+  window.onload = function() {
+    preparePage();
+  }
 </script>
 <script type="text/javascript" src="<?php echo get_stylesheet_directory(); ?>/bootstrap/js/bootstrap.min.js"></script>
 </body>
