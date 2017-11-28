0b9d1ecb67ea4aae7084cdcec08c5e73e2f7dd12
GekoCoolhard <GekoCoolhard@inbox.ru>
2017-11-28 12:00:33 +0300

    Оптимизация кода

--- templates/window_chat.php
+++ templates/window_chat.php
@@ -36,14 +36,21 @@ if ( ! empty( $_GET['event'] ) ) {
     .chat_window_avatar_img {
         height: 70%;
         background-image: url("https://skin-challenges.com/wp-content/uploads/2017/08/profile_unknown.png");
+        background-position: center;
+        background-size: contain;
+        background-repeat: no-repeat;
     }
     .chat_window_avatar_signature {
         height: 20%;
         text-align: center;
         font-size: 30px;
+        overflow: hidden;
     }
     .chat_window {
         height: 300px;
+    }
+    .modal_button {
+        text-align: center;
     }
 </style>
 <div class="chat_container container">
@@ -71,10 +78,10 @@ if ( ! empty( $_GET['event'] ) ) {
                             <form class="form-avatar" method="post" enctype="multipart/form-data">
                                 <div class="row">
                                     <div class="text-center col-md-5">
-                                        <div class="avatar col-md-12 " alt="avatar"></div>
+                                        <div class="avatar"></div>
                                         <label class="file_upload col-md-12 col-xs-12">
                                             <span class="button">Загрузить фотографию</span>
-                                            <input class="file-avatar" type="file" name="file_to_upload" id="file_to_upload" accept="image/*">
+                                            <input class="file-avatar modal_button" type="file" name="file_to_upload" id="file_to_upload" accept="image/*">
                                         </label>
                                         <input type="hidden" name="action" value="upload">
                                     </div>
@@ -82,7 +89,7 @@ if ( ! empty( $_GET['event'] ) ) {
                             </form>
                             <!-- Колонка профиля -->
                             <div class="row">
-                                <div class="col-md-9 personal-info">
+                                <div class="col-md-12 personal-info">
                                     <div class="alert alert-info alert-dismissable">
                                         <a class="panel-close close" data-dismiss="alert">×</a>
                                         <i class="fa fa-coffee"><?php echo $message; ?></i>
@@ -91,46 +98,45 @@ if ( ! empty( $_GET['event'] ) ) {
 
                                     <form method="post" class="form-horizontal">
                                         <div class="form-group">
-                                            <label class="col-lg-3 control-label">Имя:</label>
+                                            <label for="first_name" class="col-lg-3 control-label">Имя:</label>
                                             <div class="col-lg-8">
-                                                <input class="form-control" type="text" name="first_name">
+                                                <input class="form-control" type="text" name="first_name" id="first_name">
                                             </div>
                                         </div>
                                         <div class="form-group">
-                                            <label class="col-lg-3 control-label">Фамилия:</label>
+                                            <label for="last_name" class="col-lg-3 control-label">Фамилия:</label>
                                             <div class="col-lg-8">
-                                                <input class="form-control" type="text" name="last_name">
+                                                <input class="form-control" type="text" name="last_name" id="last_name">
                                             </div>
                                         </div>
                                         <div class="form-group">
-                                            <label class="col-lg-3 control-label">Почта:</label>
+                                            <label for="email" class="col-lg-3 control-label">Почта:</label>
                                             <div class="col-lg-8">
-                                                <input class="form-control" type="email" name="email">
+                                                <input class="form-control" type="email" name="email" id="email">
                                             </div>
                                         </div>
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label">Никнейм:</label>
+                                            <label for="nickname" class="col-md-3 control-label">Никнейм:</label>
                                             <div class="col-md-8">
-                                                <input class="form-control" type="text" name="nickname">
+                                                <input class="form-control" type="text" name="nickname" id="nickname">
                                             </div>
                                         </div>
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label">Пароль:</label>
+                                            <label for="password" class="col-md-3 control-label">Пароль:</label>
                                             <div class="col-md-8">
-                                                <input class="form-control password" type="password" name="password">
+                                                <input class="form-control password" type="password" name="password" id="password">
                                             </div>
                                         </div>
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label">Подтвердите пароль:</label>
+                                            <label for="confirm_password" class="col-md-3 control-label">Подтвердите пароль:</label>
                                             <div class="col-md-8">
-                                                <input class="form-control confirm_password" type="password" name="confirm_password">
+                                                <input class="form-control confirm_password" type="password" name="confirm_password" id="confirm_password">
                                             </div>
                                             <div class="message col-md-8 col-md-offset-3"></div>
                                         </div>
                                         <input type="hidden" name="action" value="edit_user_info">
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label"></label>
-                                            <div class="col-md-8">
+                                            <div class="col-md-12 modal_button">
                                                 <input type="submit" class="btn btn-primary" value="Сохранить изменения">
                                             </div>
                                         </div>
