a5f3b5c90886f4471cb39c467f603a1f05d9912e
Ruslan Ishmametov <russ-i@yandex.ru>
2017-12-02 22:13:06 +0300

    Коорекция верстки

--- templates/message.php
+++ templates/message.php
@@ -1,22 +1,19 @@
-<div class="wrapper">
-    <div class="adapchat">
-        <div class="adapchat-item">
-            <div class="adapchat-chater">
-                <img src="images/chater-1.jpg" alt="" class="adapchat-photo">
-            </div>
-            <div class="adapchat-text">
-                <div class="subject_matter">
-                    <h3>
-                        <?php echo $rows['title']; ?>
-                    </h3>
-                </div>
-                <div class="adapchat-message">
-					<?php echo $rows['content']; ?>
-                </div>
-                <div class="adapchat-date">
-					<?php echo $rows['datatime']; ?>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="adapchat-item">
+	<div class="adapchat-chater">
+		<img src="../images/character-designer.png" alt="" class="adapchat-photo">
+	</div>
+	<div class="adapchat-text">
+		<div class="subject_matter">
+			<h3>
+				<?php echo $rows['title']; ?>
+			</h3>
+		</div>
+		<div class="adapchat-message">
+			<?php echo $rows['content']; ?>
+		</div>
+		<div class="adapchat-date">
+			<?php echo $rows['datatime']; ?>
+		</div>
+	</div>
 </div>
+
